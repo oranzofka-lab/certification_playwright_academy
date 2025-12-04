4ada28eca4ce05e63e2a5e7f98644ce472a1e4c1
oranzofka <oranzofka@gmail.com>
2025-12-04 12:17:26 +0100

    jeste playwright

--- tests/api/create_account.specs.ts
+++ tests/api/create_account.specs.ts
@@ -1,3 +1,20 @@
 import { expect, test } from "@playwright/test";
 
-test("Login_user", async ({ request }) => {});
+test("Login_user", async ({ request }) => {
+  const login = await request.post(
+    "https://tegb-backend-877a0b063d29.herokuapp.com/tegb/login",
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        username: "zuzka_certifikace",
+        password: "1234",
+      },
+    }
+  );
+
+  expect(login.status()).toBe(201);
+  const response = await login.json();
+  expect(response).toHaveProperty("access_token");
+});
